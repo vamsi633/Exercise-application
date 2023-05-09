6f3f68acc7eb76e18c0d7847148ee544440ea188
vamsi633 <vamsikrishnanouluri@gmail.com>
2023-05-09 18:09:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Route,Routes} from 'react-router-dom'
+import './App.css'
+import { Box } from '@mui/material';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ExerciseDetails from './pages/ExerciseDetails';
+import Footer from './components/Footer';
+function App() {
+  
+  return(
+   <Box width="400px" sx={{width:{xl:'1488px'}}} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/exercise/:id' element={<ExerciseDetails/>}></Route>
+      </Routes>
+      <Footer />
+   </Box>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import { Box } from "@mui/material";
+import HeroBanner from "../components/HeroBanner";
+import SearchExercises from "../components/SearchExercises";
+import Exercises from "../components/Exercises";
+import { useState } from "react";
+const Home = () => {
+    const [bodyPart,setBodyPart]=useState('all');
+    const [exercises,setExercises]=useState([]);
+
+    return (
+        <Box>
+            <HeroBanner />
+            <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
+            <Exercises  setExercises={setExercises} bodyPart={bodyPart} exercises={exercises}/>
+        </Box>
+    );
+};
+
+export default Home;
